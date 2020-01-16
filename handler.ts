39966312bcd7f64a5b84beaa0c5c57796e0773f2
shuta13 <hollystarsun@gmail.com>
2020-01-16 16:15:39 +0900

    chore: .travis.yml追加、lint追加

--- handler.ts
+++ handler.ts
@@ -1,8 +1,8 @@
-require('dotenv').config();
-import 'source-map-support/register';
+require("dotenv").config();
+import "source-map-support/register";
 
-import * as https from 'https';
-import * as url from 'url';
+import * as https from "https";
+import * as url from "url";
 
 interface SlackReqOpts {
   method: string;
@@ -10,50 +10,51 @@ interface SlackReqOpts {
 }
 
 export const main = (event, _context, _callback) => {
-  const webhook_url = process.env.SLACK_WEBHOOK_URL;
-  const slack_req_opts = url.parse(webhook_url) as any as SlackReqOpts;
-  slack_req_opts.method = 'POST';
-  slack_req_opts.headers = {'Content-type': 'application/json'};
+  const webhook_url = process.env.SLACK_WEBHOOK_URL; // eslint-disable-line
+  const slack_req_opts = (url.parse(webhook_url) as any) as SlackReqOpts; // eslint-disable-line
+  slack_req_opts.method = "POST"; // eslint-disable-line
+  slack_req_opts.headers = { "Content-type": "application/json" }; // eslint-disable-line
 
   // get AWSIoT1Click event
   const { deviceEvent, placementInfo } = event as any;
   const clickType = deviceEvent.buttonClicked.clickType;
   let text = null;
   switch (clickType) {
-    case 'SINGLE':
+    case "SINGLE":
       text = placementInfo.attributes.single;
       break;
-    case 'DOUBLE':
+    case "DOUBLE":
       text = placementInfo.attributes.double;
       break;
-    case 'LONG':
+    case "LONG":
       text = placementInfo.attributes.long;
       break;
-  };
-  if(text == null) return;
+  }
+  if (text == null) return;
 
   // send message to slack
-  let status = 'dummy';
+  let status = "dummy";
   const req = https.request(slack_req_opts, res => {
     if (res.statusCode === 200) {
-      status = 'posted to slack';
-    }
-    else {
-      status = 'status code:' + res.statusCode;
+      status = "posted to slack";
+    } else {
+      status = "status code:" + res.statusCode;
     }
   });
 
-  req.on('error', e => {
-    console.log('problem with request' + e.message);
+  req.on("error", e => {
+    console.log("problem with request" + e.message);
     _context.fail(e.message);
   });
 
-  req.write(JSON.stringify({
-    text: text,
-    username: placementInfo.placementName,
-    icon_emoji: placementInfo.attributes.icon_emoji
-  }));
+  req.write(
+    JSON.stringify({
+      text: text,
+      username: placementInfo.placementName,
+      icon_emoji: placementInfo.attributes.icon_emoji // eslint-disable-line
+    })
+  );
   req.end();
 
   return status;
-}
+};
