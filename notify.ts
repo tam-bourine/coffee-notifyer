831cccf01a3400e8334384b6e7dded48328dd9bb
Asato Nago <nago@tam-bourine.co.jp>
2020-01-30 12:04:09 +0900

    chore: fix yml n add dummy jsons

--- notify.ts
+++ notify.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/camelcase */
 require("dotenv").config();
 import "source-map-support/register";
 const axios = require("axios");
