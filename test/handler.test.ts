6678bf75823f099c01e2b672262333e3d4929469
shuta13 <hollystarsun@gmail.com>
2020-01-30 12:59:35 +0900

    feat: added easy test by jest

---
+++ test/handler.test.ts
@@ -0,0 +1,10 @@
+import { dev } from "../handler";
+const _context = require("aws-lambda-mock-context"); // eslint-disable-line
+const event = require("../button-dummy.json"); // eslint-disable-line
+
+describe("handler/dev", () => {
+  it("exec dev func", () => {
+    const response = dev(event, _context);
+    expect(typeof response).toBe("string");
+  });
+});
