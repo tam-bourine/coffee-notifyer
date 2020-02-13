598b11721832c1459ab992657c9f48a1887c89ee
shuta13 <hollystarsun@gmail.com>
2020-02-13 18:36:57 +0900

    fix: fixed shell file

--- env_travis.sh
+++ env_travis.sh
@@ -1,14 +1,20 @@
-# install travis
-sudo gem install travis
-# read lines of .env file
-ENV_LINES=`cat .env | wc -l`
-COUNT=0
-# read .env and encrypt
-while [ $COUNT -ne $ENV_LINES ]
-do
-  COUNT=`expr $COUNT + 1`
-  SED_LINE=${COUNT}p
-  VARIABLE_URL=`sed -n $SED_LINE .env`
-  travis encrypt $VARIABLE_URL --add env.global
-done
-echo ENV VALIABLES ENCRYPTED
+# read .travis.yml
+TRAVIS_GLOBAL=`cat .travis.yml | grep global:`
+TRAVIS_ENV=`cat .travis.yml | grep env:`
+if [ -z "$TRAVIS_ENV" ] && [ -z "$TRAVIS_GLOBAL" ]; then
+  # install travis
+  sudo gem install travis
+  # read lines of .env file
+  ENV_LINES=`cat .env | wc -l`
+  COUNT=0
+  # read .env and encrypt
+  while [ $COUNT -ne $ENV_LINES ]
+  do
+    COUNT=`expr $COUNT + 1`
+    SED_LINE=${COUNT}p
+    VARIABLE_URL=`sed -n $SED_LINE .env`
+    travis encrypt $VARIABLE_URL --add env.global
+  done
+  echo ENV VALIABLES ENCRYPTED
+fi
+echo ALREADY VALIABLES ENCRYPTED
